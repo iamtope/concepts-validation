4bbb7b40be2d07194cd3e3a2197f956181914125
ABASS ADAMO <adamoabasstope@yahoo.com>
2024-02-16 19:07:57 +0100

    more commit

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-dotenv.config({ path: './config/.env' });
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
 
 connectDB();
 
-import indexRouter from './routes/index.js';
-import urlsRouter from './routes/urls.js';
+import indexRouter from "./routes/index.js";
+import urlsRouter from "./routes/urls.js";
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/api', urlsRouter);
+app.use("/", indexRouter);
+app.use("/api", urlsRouter);
 
 // Server Setup
 const PORT = process.env.PORT || 3333;
+app.get("/", (req, res) => {
+  res.send("Welcome to url shortener");
+});
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
