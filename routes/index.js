780226a0578264a547653fa0727f1b7c7573dbf1
ABASS ADAMO <adamoabasstope@yahoo.com>
2024-02-16 18:53:30 +0100

    init

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Url from "../models/Url.js";
+import open, { openApp, apps } from "open";
+
+const router = express.Router();
+
+router.get("/:urlId", async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    if (url) {
+      await Url.updateOne(
+        {
+          urlId: req.params.urlId,
+        },
+        { $inc: { clicks: 1 } }
+      );
+      console.log(url.origUrl);
+      await open(url.origUrl);
+    } else res.status(404).json("Not found");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server Error");
+  }
+});
+
+export default router;
